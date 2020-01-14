bdc337f7fdced471ccbea0673de656c6fe02f277
Kevinfl <kevinfl@hotmail.com>
2020-01-14 18:28:50 +0100

    upload
    js

---
+++ javadetail.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector("button");
+var background = document.querySelector(".detail-article");
+
+btn.addEventListener("click", function(){
+   background.classList.toggle("active");
+
+
+});
