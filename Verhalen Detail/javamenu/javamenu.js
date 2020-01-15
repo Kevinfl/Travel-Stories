33b66eb0458cd5f780ef02fecc66e71151c888f5
Kevinfl <kevinfl@hotmail.com>
2020-01-15 10:36:25 +0100

    Delete javamenu.js

--- Verhalen Detail/javamenu/javamenu.js
+++
@@ -1,7 +0,0 @@
-var btn = document.querySelector("button");
-var navMain = document.querySelector(".nav-main");
-
-btn.addEventListener("click", function(){
-   navMain.classList.toggle("block");
-
-});
