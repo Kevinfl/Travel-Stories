92718ca83800a05dcad76a93c14040f034310ca7
Kevinfl <kevinfl@hotmail.com>
2020-01-15 10:36:32 +0100

    Delete javadetail.js

--- Verhalen Detail/java /javadetail.js
+++
@@ -1,8 +0,0 @@
-var btn = document.querySelector("button");
-var background = document.querySelector(".detail-article");
-
-btn.addEventListener("click", function(){
-   background.classList.toggle("active");
-
-
-});
